1b40b6c9b5a5b09a8383dd8d4c8d63d95cefad88
Dan-George Filimon <dangeorge.filimon@gmail.com>
2010-10-03 11:56:24 +0300

    Cleaning up old files 2

--- ClientGui.py
+++
@@ -1,9 +0,0 @@
-from PyQt4.QtGui import *
-from PyQt4.QtCore import *
-
-import QuestionView
-import QuestionGrid
-
-class ClientGui(QWidget):
-    def __init__(self, parent=None):
-        super(ClientGui, self).__init__(parent)
